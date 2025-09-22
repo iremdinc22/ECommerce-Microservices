69133614a3787be2270a02da8179d10e0494b99e
iremdinc22 <iremdinc211@gmail.com>
2025-09-22 10:58:07 +0300

    chore(identity): before duende-is-ui

---
+++ IdentityServer/MultiShop.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MultiShop.IdentityServer.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
