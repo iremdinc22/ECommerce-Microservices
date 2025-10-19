865373f42a932a151102595208b03109f0d30674
iremdinc22 <iremdinc211@gmail.com>
2025-10-19 20:21:14 +0300

    feat: implement comment submission on Product Detail page
    - Added review form UI and moved comments section to product detail
    - Fixed null model error and added validation for fields
    - Integrated API call to create new comments with rating and metadata
    - Improved UX with star rating rendering and success/error feedback

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,20 +1,56 @@
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
+   
     public class ProductListController : Controller
     {
-        public ActionResult Index(string id)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index(string id)
         {
             ViewBag.i = id;
             return View();
         }
         
-        public ActionResult ProductDetail(string id)
+        public IActionResult ProductDetail(string id)
         {
             ViewBag.x = id;
             return View();
         }
+        
+        [HttpGet]
+        public async Task  <PartialViewResult> AddComment(string id)
+        {
+            return PartialView();
+        }
+        
+        [HttpPost]
+        public async Task <IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ImageUrl = "test";
+            createCommentDto.Rating = 1;
+            createCommentDto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            createCommentDto.Status = false;
+            createCommentDto.ProductId = "68ca9d589220c249f18ef89b";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCommentDto);
+            StringContent stringcontent = new StringContent(jsonData , Encoding.UTF8 , "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5013/api/Comments", stringcontent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+
 
 
     }
